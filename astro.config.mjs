8a7e60b93f8a43e9eb17ea9809459f5ac76c0abc
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2026-05-17 17:04:21 -0700

    add sitemap + robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://dcrader-template-dentists.vercel.app',
+  integrations: [sitemap()],
   output: 'static',
   compressHTML: true,
   prefetch: {
